56253035547ecf4a6f70ba2581fb7aedca3f8adb
blalp <26471229+blalp@users.noreply.github.com>
2020-09-04 06:03:41 -0700

    added other platforms

--- src/main/java/com/blalp/sftwrapper/platforms/Generic/GenericConfig.java
+++ src/main/java/com/blalp/sftwrapper/platforms/Generic/GenericConfig.java
@@ -4,6 +4,11 @@ import java.io.File;
 
 import com.blalp.sftwrapper.interfaces.IPath;
 import com.blalp.sftwrapper.platforms.Linux.LinuxConfig;
+import com.blalp.sftwrapper.platforms.Mac.MacConfigs;
+import com.blalp.sftwrapper.platforms.Windows.WindowsConfigs;
+import com.blalp.sftwrapper.util.UserErrorExepction;
+
+import oshi.SystemInfo;
 
 /**
  * GenericConfig
@@ -20,7 +25,25 @@ public abstract class GenericConfig implements IPath {
     }
 
     public static IPath getRevelventPath(){
-        return new LinuxConfig();
+        switch(SystemInfo.getCurrentPlatformEnum()){
+            case FREEBSD:
+            case LINUX:
+            case SOLARIS:
+            case UNKNOWN:
+            return new LinuxConfig();
+            case MACOSX:
+            return new MacConfigs();
+            case WINDOWS:
+            return new WindowsConfigs();
+            default:
+            try {
+                throw new UserErrorExepction("unsupported platform");
+            } catch (UserErrorExepction e) {
+                e.printStackTrace();
+            }
+            System.exit(-1);
+            return null;
+        }
     }
     public void mkdirs() {
         new File(getPathInstanceCache()).mkdirs();
